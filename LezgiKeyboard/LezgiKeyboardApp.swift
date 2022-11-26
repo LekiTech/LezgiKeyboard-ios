03ede796a6175a579689a809a7b7851cae54e0f1
ktadzjibov <ktadzjibov@gmail.com>
2022-11-26 23:25:59 +0100

    Initial Commit

---
+++ LezgiKeyboard/LezgiKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LezgiKeyboardApp.swift
+//  LezgiKeyboard
+//
+//  Created by Kamran Tadzjibov on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LezgiKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
